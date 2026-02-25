d618a568ee35fb72d9b24fd38de89dedf6f62c50
Omkar-Akul <omkarakul1907@gmail.com>
2026-02-26 00:50:17 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     const tl = gsap.timeline();
 
     tl.to(".vi-mask-group", {
-      rotate: 10,
+      rotate: 45,
       duration: 2,
       ease: "Power4.easeInOut",
       transformOrigin: "50% 50%",
@@ -32,6 +32,48 @@ function App() {
   });
 
   useGSAP(()=>{
+
+     gsap.to(".main", {
+      scale: 1,
+      rotate: 0,
+      duration: 2.4,
+      delay: -1.3,
+      ease: "Expo.easeInOut",
+    });
+    gsap.to(".sky", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 2,
+      delay: -.8,
+      ease: "Expo.easeInOut",
+    });
+
+    gsap.to(".bg", {
+      scale: 1.1,
+      rotate: 0,
+      duration: 2,
+      delay: -.8,
+      ease: "Expo.easeInOut",
+    });
+
+     gsap.to(".character", {
+      scale: 0.8,
+      x: "-50%",
+      bottom: "-63%",
+      rotate: 0,
+      duration: 2,
+      delay: -.8,
+      ease: "Expo.easeInOut",
+    });
+
+      gsap.to(".text", {
+      scale: 1,
+      rotate: 0,
+      duration: 2,
+      delay: "-.8",
+      ease: "Expo.easeInOut",
+    });
+
     const main = document.querySelector(".main");
 
     main?.addEventListener("mousemove",function(e){
@@ -73,7 +115,7 @@ function App() {
               </g>
             </mask>
           </defs>
-          <image
+          <image  
             href="./bg.png"
             width="100%"
             height="100%"
@@ -83,8 +125,8 @@ function App() {
         </svg>
       </div>
       {showContent && (
-        <div className="main w-full bg-black">
-          <div className="landing w-full h-screen bg-black">
+        <div className="main w-full -rotate-20 scale-200  bg-black">
+          <div className="landing overflow-hidden relative w-full h-screen bg-black">
                  <div className="navbar absolute top-0 left-0 z-10 w-full py-10 px-10">
               <div className="logo flex gap-6">
                 <div className="lines flex flex-col gap-1">
@@ -103,14 +145,14 @@ function App() {
               <h1>auto</h1>
             </div>
             <div className="imagesdiv relative overflow-hidden w-full h-screen ">
-                <img className=" sky scale-105 absolute top-0 l-0 w-full h-full object-cover" src="../sky.png" alt="" />
-              <img className=" bg scale-105 absolute top-0 l-0 w-full h-full object-cover" src="../bg.png" alt="" />
-                 <div className="text text-white absolute top-10 left-1/2 flex flex-col gap-1 -translate-x-1/4">
-              <h1 className="text-8xl -ml-30">grand</h1>
-              <h1 className="text-8xl ml-20">theft</h1>
-              <h1 className="text-8xl -ml-30">auto</h1>
+                <img className=" sky scale-200  -rotate-20 absolute top-0 l-0 w-full h-full object-cover" src="../sky.png" alt="" />
+              <img className=" bg scale-200 -rotate-20 absolute top-0 l-0 w-full h-full object-cover" src="../bg.png" alt="" />
+                 <div className="text scale-150 -rotate-12 text-white absolute top-10 left-1/2 flex flex-col gap-0.5 -translate-x-1/4">
+              <h1 className="text-8xl -ml-40">grand</h1>
+              <h1 className="text-8xl ml-0">theft</h1>
+              <h1 className="text-8xl -ml-40">auto</h1>
             </div>
-              <img className=" character absolute top-8 -left-0.5 translate-x-1/2 scale-80" src="../girlbg.png" alt="" />
+              <img className="absolute character -bottom-[250%] left-1/2 -translate-x-1/2  scale-[3] -rotate-20" src="../girlbg.png" alt="" />
               
             </div>
             <div className="btmbar text-white absolute bottom-0 left-0 w-full py-12 px-10 bg-linear-to-t from-black to-transparent ">
@@ -126,14 +168,17 @@ function App() {
           <div className=" w-full h-screen flex items-center justify-center bg-black ">
             <div className="cntnr  flex text-white w-full h-9/12">
             <div className="limg relative w-1/2 h-full ">
-              <img className=" scale-100 ml-28 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " src="../imag.png" alt="" />
+              <img className=" scale-100 ml-20 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 " src="../imag.png" alt="" />
             </div>
              <div className="rimg">
-              <h1 className="text-white text-7xl">still running </h1>
-              <h1 className="text-white text-7xl">not hunting </h1>
-              <p className="mt-10"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti maxime, animi 
-                cupiditate nesciunt libero,ratione, doloribus eligendi pariatur accusantium porro ipsa magnam iusto?</p>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate tempora voluptatibus voluptatem esse?</p>
+              <h1 className="text-white text-7xl ml-20">still running </h1>
+              <h1 className="text-white text-7xl ml-80 mt-2">not hunting </h1>
+              <p className="mt-10 text-2xl font-[Helvetica_Now_Display]"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti maxime, animi  </p>
+                <p className="mt-0 text-2xl font-[Helvetica_Now_Display]">cupiditate nesciunt libero,ratione, doloribus eligendi pariatur accusantium porro ipsa magnam iusto?</p>
+              <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate tempora voluptatibus voluptatem esse?</p><p className="mt-0 text-2xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum voluptatem illo itaque recusandae quis omnis libero dolores hic quasi?</p>
+               <button className="bg-yellow-500 px-10 py-10 text-black mt-10 text-4xl ml-50">
+                  Download Now
+                </button>
               </div>
              </div>
           </div>
